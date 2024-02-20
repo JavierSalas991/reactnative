616e9cf27eaf1c8c098ee5503fcfca9bf2645c52
Javier Salas <javier.salas.991@gmail.com>
2024-02-20 10:29:02 -0300

    sabe

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native-web';
+
+const Main = () => {
+    return (
+        <View>
+            <Text>Texto ejemplo</Text>
+        </View>
+    );
+};
+
+export default Main;
